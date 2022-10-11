1980df6b28ff8cdb6fe5db12484c2f85c9c2dfb3
GunnarMoffit <100969144+GunnarMoffit@users.noreply.github.com>
2022-10-11 00:23:12 -0500

    Update driver.c

--- assign2/src/driver.c
+++ assign2/src/driver.c
@@ -7,14 +7,14 @@
 int main() {
     if (!yyparse()){
         printAst(ast, 1);
-/*
+
         printf("\n\nSYMBOL TABLE:\n");
         for(int i = 0; i < MAXIDS; i++){
             if(strTable[i].id != NULL){
                 output_entry(i);
             }
         }
-*/
+
     }
     return 0;
 }
