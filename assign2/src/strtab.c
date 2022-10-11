3d0c6d10ccc290ef8de56d2ceefe014a275d1137
GunnarMoffit <100969144+GunnarMoffit@users.noreply.github.com>
2022-10-10 23:43:15 -0500

    hash table implementation

--- assign2/src/strtab.c
+++ assign2/src/strtab.c
@@ -17,19 +17,51 @@ unsigned long hash(unsigned char *str)
 }
 
 int ST_insert(char *id, char *scope, int data_type, int symbol_type){
-    // TODO: Concatenate the scope and id and use that to create the hash key
-
+	int index = 0;
+    //Concatenate the scope and id and use that to create the hash key
+	unsigned char *key = sprintf(id, scope);
+	index = ST_lookup(id,scope);
     // TODO: Use ST_lookup to check if the id is already in the symbol table. If yes, ST_lookup will return an index that is not -1. if index != -1, that means the variable is already in the hashtable. Hence, no need to insert that variable again. However, if index == -1, then use linear probing to find an empty spot and insert there. Then return that index.
+	if(index == -1){
+		index = hash(key);
+		while(strTable[index].id != NULL){
+			index++;
+		}
+		//place new entry into the empty spot
+		strTable[index].id = id;
+		strTable[index].scope = scope;
+		strTable[index].data_type = data_type;
+		strTable[index].symbol_type = symbol_type;
 
+	}
+	else{
+		// id is already there so do nothing
+	}
     return index;
 }
 
 int ST_lookup(char *id, char *scope) {
-    // TODO: Concatenate the scope and id and use that to create the hash key
+    // sets index to -1 initially
+	int index = -1;
+	//Concatenate the scope and id and use that to create the hash key
+	unsigned char *key = sprintf(id, scope);
+	int position = hash(key);
 
-
-    // TODO: Use the hash value to check if the index position has the "id". If not, keep looking for id until you find an empty spot. If you find "id", return that index. If you arrive at an empty spot, that means "id" is not there. Then return -1. 
-
+	if(strTable[position].id == id){
+		index = position;
+		return index;
+	}
+	else{
+		//iterate through the hash table until either the id is found or id is empty
+		while(strTable[position].id != NULL){
+			position++;
+			if(strTable[position].id == id){
+				index = position;
+				return index;
+			}
+		}
+	}
+	//if the id is empty in the hash table function will return -1 as id will not in the table
     return index;
 }
 
