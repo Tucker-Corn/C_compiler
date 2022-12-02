ae7269a055076d3882b3eac83c71ceb106b3dad2
Tucker Corn <tec74@txstate.edu>
2022-12-02 17:43:55 -0600

    working ex

---
+++ assign4/src/gencode.c
@@ -0,0 +1,145 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <tree.h>   // definition for nodes of the AST
+#include <strtab.h> // the symbol table for needed information about nodes
+
+// a counter for jump labels to be made unique
+int labelcounter=0;
+// reset register counter (done after calling a func or at beginning of func)
+int registercounter=7;
+
+// defines to get the next label counter or to reset registers
+#define nextlabel() (++labelcounter)
+#define resetregisters() (registercounter=7)
+
+/*
+An example of how / why for label counters
+
+Suppose we have:
+FORNODE
+FORNODE->children[0] = stmtnode (init)
+FORNODE->children[1] = condnode (condition)
+FORNODE->children[2] = compoundstmt (body)
+FORNODE->children[3] = stmtnode (post)
+
+void emitforstmt(tree *ast,FILE *outfile) {
+  // Get a unique label number for the labels of this FORSTMT
+  const int forlabel = nextlabel();
+
+  // emit the for statement:
+  fprintf(outfile,"FORSTMT_INIT%d: # Preparing to do for loop\n",
+                  forlabel);
+  gencode(ast->children[0],outfile);
+  fprintf(outfile,"FORSTMT_COND%d: # For statement condition\n",
+                  forlabel);
+  // how you evaluate conditions will probably vary
+  const int result = gencode(ast->children[1],outfile);
+  // here I am assuming this register will equal zero if the condition is false
+  fprintf(outfile,"beq %s, $zero, FORSTMT_END%d # Exit the for statement\n",
+                  registernames[result], forlabel);
+  // otherwise we do the body of the for loop
+  fprintf(outfile,"FORSTMT_BODY%d: # Begin the for statement\'s body\n",
+                  forlabel);
+  // emit the body of the loop
+  gencode(ast->children[2],outfile);
+  // do the post expression
+  fprintf(outfile,"FORSTMT_POST%d: # For statement post statement\n",
+                  forlabel);
+  // We need to return to the for loop's condition
+  fprintf(outfile,"j FORSTMT_COND%d # Return to for condition\n",
+                  forlabel);
+  // Now we print the label for the for statement exit
+  fprintf(outfile,"FORSTMT_END%d: # End of for loop\n",
+                  forlabel);
+}
+int gencode(tree *ast,FILE *outfile) {
+  switch(ast->nodeKind) {
+  case FORSTMT:
+    emitforstmt(ast,outfile); return 0;
+  }
+  // if we don't return a value in the switch statement then it wasn't handled
+  // returning -1 should cause a seg-fault if used to index registernames[]
+  return -1;
+}
+*/
+
+/*
+ * The following page is a good reference for MIPS
+https://ecs-network.serv.pacific.edu/ecpe-170/tutorials/mips-instruction-set
+*/
+int gencode(tree *ast,FILE *outfile) {
+  // variables can't be defined inside a switch
+  // move more complicated things to their own function to keep this cleaner
+  // (e.g., function definition needs to setup/teardown stack, ...)
+  int lhs,rhs,dst;
+  switch(ast->nodeKind) {
+// output(6);
+    case INTCONST:
+      dst = getregister();
+      fprintf(outfile,"li %s, %d\n",
+                      registernames[dst], ast->value);
+      return dst;
+/*
+ *     FUNCCALLNODE
+ *     ARGLIST
+ *     EXPRESSION , ....
+ */
+    case FUNCDEFNODE:
+      setupframe();
+      resetregisters();
+      for all statements in funcdefnode emitcode();
+      teardownframe_andrestore();
+      setreturnvalue();
+      if (strcmp(funcname,"main")==0)
+        syscall_for_clean_exit();
+      else {
+        fprintf(outfile,"jr $ra\n");
+      }
+    case FUNCCALLNODE:
+      if (strcmp(ast->id,"output")) {
+        rhs = gencode(ast->children[0]->children[0]);
+        printf("li $v0, 1\n");
+        printf("mov $a0, %s\n",registernames[rhs]);
+        printf("syscall\n");
+      }
+      else {
+        // save registers, prepare to move context
+        // resolve arguments, put them where appropriate
+        // jump and link to function label
+        // immediately after jump and link all $t_ registers are garbage.
+        // return value will be (somewhere)
+        resetregisters();
+        // anything needed will have to be restored from the stack
+      }
+      // return 0, break, (?)
+      return 0;
+      break;
+    default: break;
+  }
+  return -1; // (?)
+}
+
+/*
+Number  | Name      | Description
+0       | $zero     | The value 0
+2-3     | $v0 - $v1 | (values) from expression evaluation and function results
+4-7     | $a0 - $a3 | (arguments) First four parameters for subroutine
+8-15    | $t0 - $t7 | Temporary variables
+16-23   | $s0 - $s7 | Saved values representing final computed results
+24-25   | $t8 - $t9 | Temporary variables2
+31      | $ra       |Return address
+*/
+char *registernames[] = { "$zero","$at","$v0","$v1","$a0","$a1","$a2","$a3",
+                          "$t0","$t1","$t2","$t3","$t4","$t5","$t6","$t7",
+                          "$s0","$s1","$s2","$s3","$s4","$s5","$s6","$s7",
+                          "$t8","$t9","$k0","$k1","$gp","$sp","$fp","$ra" };
+// get the next available register
+// this naiively just cycles through $t0 -> $t9 -> $t0
+int nextreg() {
+  // jump the gap from $t7 to $t8
+  if (++registercounter == 16) registercounter = 24;
+  // wrap from $t9 to $t0
+  else if (registercounter == 26) registercounter = 8;
+  return registercounter;
+}
--- assign4/src/tree.c
+++ assign4/src/tree.c
@@ -1,157 +1,142 @@
-#include<tree.h>
-#include<strtab.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <tree.h>
+#include <table.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* string values for ast node types, makes tree output more readable */
-char *nodeNames[33] = {"program", "declList", "decl", "varDecl", "typeSpecifier",
-                       "funDecl", "formalDeclList", "formalDecl", "funBody",
-                       "localDeclList", "statementList", "statement", "compoundStmt",
-                       "assignStmt", "condStmt", "loopStmt", "returnStmt","expression",
-                       "relop", "addExpr", "addop", "term", "mulop", "factor",
-                       "funcCallExpr", "argList", "integer", "identifier", "var",
-                       "arrayDecl", "char", "funcTypeName"};
-
-char *typeNames[3] = {"int", "char", "void"};
-char *ops[10] = {"+", "-", "*", "/", "<", "<=", "==", ">=", ">", "!="};
+char *nodeNames[70] = {"program", "declList", "decl", "varDecl", "localVarDecl", "typeSpecifier",
+                      "funDecl", "formalDeclList", "formalDecl", "funBody",
+                      "localDeclList", "statementList", "statement", "compoundStmt",
+                      "assignStmt", "condStmt", "loopStmt", "returnStmt", "var",
+                      "expression", "relop", "addExpr", "addop", "term", "mulop",
+                      "factor", "funcCallExpr", "argList", "if", "else",
+                      "while", "return", "+", "-", "*", "/", "<", ">", ">=", "<=", "==", "!=",
+                      "=", "[", "]", "{", "}", "(", ")", ",", ";", "intconst",
+                      "charconst", "strconst", "identifier", "error", "illegal token",
+                      "unknown", "int", "void", "char", "string", "function", "int[]", "void[]", "char[]"};
 
 tree *maketree(int kind) {
-      tree *this = (tree *) malloc(sizeof(struct treenode));
-      this->nodeKind = kind;
-      this->numChildren = 0;
-      return this;
+  tree *this = (tree *) malloc(sizeof(struct treenode));
+  this->nodeKind = kind;
+  this->numChildren = 0;
+  return this;
+
 }
 
 tree *maketreeWithVal(int kind, int val) {
-      tree *this = (tree *) malloc(sizeof(struct treenode));
-      this->nodeKind = kind;
-      this->numChildren = 0;
-      this->val = val;
-      return this;
+  tree *this = (tree *) malloc(sizeof(struct treenode));
+  this->nodeKind = kind;
+  this->numChildren = 0;
+  this->val = val;
+  return this;
+
 }
 
-tree * maketreeWithNameVal(int kind, char *name)
-{
-    tree *this = (tree *) malloc(sizeof(struct treenode));
-    this->nodeKind = kind;
-    this->numChildren = 0;
-    this->name = name;
-
-    return this;
+void addChild(tree *parent, tree *child) {
+  if (parent->numChildren == MAXCHILDREN) {
+    printf("Cannot add child to parent node\n");
+    exit(1);
+  }
+  nextAvailChild(parent) = child;
+  parent->numChildren++;
 }
 
-tree * maketreeWithChild(int kind, tree *child)
-{
-    tree *this = (tree *) malloc(sizeof(struct treenode));
-    this->nodeKind = kind;
-    addChild(this, child);
-    this->numChildren++; /* added this increment during codegen */
-    return this;
-}
-
-
-void addChild(tree *parent, tree *child)
-{
-      if (parent->numChildren == MAXCHILDREN)
-      {
-          printf("Cannot add child to parent node\n");
-          exit(1);
-      }
-    if(child)
-    {
-        nextAvailChild(parent) = child;
-        parent->numChildren++;
-        child->parent = parent;
-    }
-}
-
-tree * ChildByKind(tree *node, char *kind)
-{
-    tree * localNode = NULL;
-    int i;
-    for (i = 0; i < node->numChildren; i++)
-    {
-        if (strcmp(kind, nodeNames[node->children[i]->nodeKind]) == 0)
-        {
-            localNode = node->children[i];
-        }
-    }
-    return localNode;
-}
-
-tree * NestedChildByKind(tree *node, char *kind, int nestLevel)
-{
-    tree * localNode = NULL;
-    if (strcmp(nodeNames[node->nodeKind], kind) == 0)
-    {
-        return node;
-    }
-    int i;
-    for (i = 0; i < node->numChildren && localNode == NULL; i++)
-    {
-        localNode = NestedChildByKind(getChild(node, i), kind, nestLevel + 1);
-    }
-    return localNode;
-}
-
-
-int isNodeName(tree * node, char *kind)
-{
-    int isKind = strcmp(getNodeName(node), kind);
-    return isKind;
-}
-
-/*recursively count a kind of node in a given tree. */
-int walkSubTree(tree *node, char* kind, int nestLevel)
-{
-    int count = 0;
-    if (strcmp(nodeNames[node->nodeKind], kind) == 0)
-    {
-        count++;
-    }
-    int i;
-    for (i = 0; i < node->numChildren; i++)
-    { count += walkSubTree(getChild(node, i), kind, nestLevel + 1); }
-    return count;
-}
 void printAst(tree *node, int nestLevel) {
-      char* nodeName = nodeNames[node->nodeKind];
-      if(strcmp(nodeName,"identifier") == 0){
-          if(node->val == -1)
-              printf("%s,%s\n", nodeName,"undeclared variable");
-          else
-              printf("%s,%s\n", nodeName,get_symbol_id(node->val));
-      }
-      else if(strcmp(nodeName,"integer") == 0){
-          printf("%s,%d\n", nodeName,node->val);
-      }
-      else if(strcmp(nodeName,"char") == 0){
-          printf("%s,%c\n", nodeName,node->val);
-      }
-      else if(strcmp(nodeName,"typeSpecifier") == 0){
-          printf("%s,%s\n", nodeName,typeNames[node->val]);
-      }
-      else if(strcmp(nodeName,"relop") == 0 || strcmp(nodeName,"mulop") == 0 || strcmp(nodeName,"addop") == 0){
-          printf("%s,%s\n", nodeName,ops[node->val]);
-      }
-      else{
-          printf("%s\n", nodeName);
-      }
+	switch (node->nodeKind) {
+		case T_INTCONST:
+			printf("<integer: %d>\n", node->val);
+			break;
+		case T_CHARCONST:
+			printf("<character: '%c'>\n", node->val);
+			break;
+		case T_STRCONST:
+			printf("<string: \"%s\">\n", symbolTable[node->val].name);
+			break;
+		case T_ID:
+			if (node->val == -1) {
+				printf("<Undeclared var/fun>\n");
+			} else {
+				if (symbolTable[node->val].type == T_FUNCTION) 
+				  printf("<identifier: \"%s\", %s, %s>\n", symbolTable[node->val].name, nodeNames[symbolTable[node->val].returnType], symbolTable[node->val].scope);
+				else
+				  printf("<identifier: \"%s\", %s, %s>\n", symbolTable[node->val].name, nodeNames[symbolTable[node->val].type], symbolTable[node->val].scope);
+			}
+			break;
+		default:
+			if (node->numChildren == 0 && node->val != -1) {
+				printf("<%s>\n", nodeNames[node->nodeKind]);
+			} else {
+				printf("%s\n", nodeNames[node->nodeKind]);
+			}
+	}
 
-      int i, j;
+	int i, j;
 
-      for (i = 0; i < node->numChildren; i++)  {
-          for (j = 0; j < nestLevel; j++)
-              printf("    ");
-          printAst(getChild(node, i), nestLevel + 1);
-      }
-
+	for (i = 0; i < node->numChildren; i++)  {
+		for (j = 0; j < nestLevel; j++) 
+			printf("    ");
+		printAst(getChild(node, i), nestLevel + 1);
+	}
 }
 
-void flattenList(tree *list, tree *subList){
-    for(int i=0; i < subList->numChildren; i++){
-        addChild(list,getChild(subList,i));
+void printAst2(tree *node, int nestLevel) {
+	
+    int i, j;
+    printf ("%s\t%d\n", nodeNames[node->nodeKind], node->val);
+    for (i = 0; i < node->numChildren; i++) 
+    {
+        for (j = 0; j < (nestLevel - 1); j++)
+            printf ("| ");
+        if (nestLevel > 0)
+            printf ("+ ");
+        printAst2 (getChild (node, i), nestLevel + 1);
     }
+}
+
+void printCode(tree *node, int nestLevel) {
+	
+	int i, j;
+
+	switch (node->nodeKind) {
+		case T_INTCONST:
+			printf("%d", node->val);
+			break;
+		case T_CHARCONST:
+			printf("'%c'", node->val);
+			break;
+		case T_STRCONST:
+			printf("\"%s\"", symbolTable[node->val].name);
+			break;
+		case T_ID:
+			if (node->val == -1) {
+				printf("<unknown> ");
+			} else {
+				printf("%s ", symbolTable[node->val].name);
+			}
+			break;
+		case T_LPAREN:
+			printf("%s", nodeNames[node->nodeKind]);
+			break;
+		case T_LSQ_BRKT:
+			printf("%s", nodeNames[node->nodeKind]);
+			break;
+		case T_LCRLY_BRKT:
+			printf("%s\n", nodeNames[node->nodeKind]);
+			break;
+		case T_RCRLY_BRKT:
+			printf("%s\n\n", nodeNames[node->nodeKind]);
+			break;
+		case T_SEMICLN:
+			printf("%s\n", nodeNames[node->nodeKind]);
+			break;
+		default:
+			if (node->numChildren == 0 && node->val != -1) {
+				printf("%s ", nodeNames[node->nodeKind]);
+			}	
+		}
+
+	for (i = 0; i < node->numChildren; i++)  {
+		printCode(getChild(node, i), nestLevel);
+	}
 }
 
