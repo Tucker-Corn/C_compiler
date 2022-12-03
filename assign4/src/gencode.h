c81fb7e555fc850a51774e5fc2ca08104ff3cf2f
Tucker Corn <tec74@txstate.edu>
2022-12-02 22:27:38 -0600

    Update gencode.h

--- assign4/src/gencode.h
+++ assign4/src/gencode.h
@@ -2,6 +2,10 @@
 #define GENCODE_H
 
 
-
+int Expression(tree *ast);
 int gencode(tree *ast,FILE *outfile);
-
+void emitforstmt(tree *ast,FILE *outfile);
+int getCondNode(ast->nodeKind);
+int getElseClause(ast->nodeKind);
+int getThenClause(ast->nodeKind);
+int nextreg();
